881947c359fa7486c75fe2b5d42fdd505037ab23
Your Name <you@example.com>
2025-10-25 21:08:56 +0300

    aeQGEqeyghHYGQEGT

--- backend/authentification_service/tests/test_auth.py
+++ backend/authentification_service/tests/test_auth.py
@@ -21,6 +21,10 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Переопределяем engine для тестов
+import database
+database.engine = engine
+
 def override_get_db():
     try:
         db = TestingSessionLocal()
